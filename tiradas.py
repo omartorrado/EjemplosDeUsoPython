8b5e5de198c94ec29d6f76f2abfa2bcfde0c98c6
omartorrado <frikimoro@gmail.com>
2017-10-03 14:01:16 +0200

    iniciando la clase tiradas para usar tiradas del estilo 3D6, 6D10, etc

---
+++ tiradas.py
@@ -0,0 +1,21 @@
+import random
+
+def tiradaVariosDados(dados,caras):
+    resultados=[]
+    for x in range (1,dados+1):
+        resultados[x]=random.randrange(1,caras+1)
+    return resultados
+
+def tiradaMediaDados(dados,caras):
+    resultados=0
+    for x in range(1,dados+1):
+        numero=random.randrange(1,caras+1)
+        print(numero)
+        resultados+=numero
+        print("Resultados "+str(resultados))
+    print("Media truncada "+str(int(resultados/dados)))
+    print("Modulo "+str(resultados%dados))
+    #el modulo no es lo que me interesa, sino comprobar el valor del resto en la division
+    return round(resultados/dados)
+
+tiradaMediaDados(3,10)
