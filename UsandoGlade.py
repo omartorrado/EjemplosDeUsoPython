e8cdd97b59f42bef4d6590867cc9bc720d4144a1
omartorrado <frikimoro@gmail.com>
2017-10-27 10:28:04 +0200

    aprendiendo gtk

--- UsandoGlade.py
+++ UsandoGlade.py
@@ -1,7 +1,7 @@
 import gi
+import time
 gi.require_version("Gtk",'3.0')
 from gi.repository import Gtk
-
 
 class MainWindow:
 
@@ -9,8 +9,26 @@ class MainWindow:
         ruta= "/home/local/DANIELCASTELAO/otorradomiguez/PycharmProjects/EjemplosDeUso/UsandoGlade.glade"
         builder = Gtk.Builder()
         builder.add_from_file(ruta)
-        #self.ventana=builder.get_object("ventana1")
+        self.ventana=builder.get_object("ventana1")
+        #Comentamos la siguiente linea pq ya indicamos
         #self.ventana.show_all()
+        self.txtSaudo=builder.get_object("txtSaudo")
+        self.labelSaudo=builder.get_object("labelSaudo")
+        self.boton1=builder.get_object()
+        #señales(eventos)
+        eventos={"MainWindow_Destroy":Gtk.main_quit,"boton1Clicked":self.boton1Clicked,"boton2Clicked":self.boton2Clicked}
+        builder.connect_signals(eventos)
+
+    #Gtk button devuelve un argumento indicando el boton usado (o su posicion en memoria)
+    def boton1Clicked(self,event):
+        #print(event)
+        print("Hola")
+        self.labelSaudo.set_text(self.txtSaudo.get_text()+" Bienvenido")
+        self.txtSaudo.set_text("")
+
+    def boton2Clicked(self,event):
+        #print(event)
+        print("No hago nada")
 
 if __name__=="__main__":
     MainWindow()
