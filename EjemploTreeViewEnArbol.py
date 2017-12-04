e2d51e82c39031b90bec6f06c3b9a841028345f5
omartorrado <frikimoro@gmail.com>
2017-12-04 13:24:08 +0100

    aprendiendo gtk

---
+++ EjemploTreeViewEnArbol.py
@@ -0,0 +1,35 @@
+import gi
+gi.require_version("Gtk","3.0")
+from gi.repository import Gtk
+
+class VentanaPrincipal(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Ejemplo de TreeView en arbol")
+        self.set_default_size(800,600)
+        self.set_border_width(10)
+
+        modelo = Gtk.TreeStore(str,int)
+        for abuelo in range(5):
+            punteroAbuelo= modelo.append(None,["Abuelo",abuelo])
+            for padre in range(3):
+                punteroPadre= modelo.append(punteroAbuelo,["Padre %i del abuelo %i" % (padre,abuelo),padre])
+                for hijo in range(4):
+                    punteroHijo=modelo.append(punteroPadre,["Hijo %i del padre %i del abuelo %i" % (hijo,padre,abuelo),hijo])
+
+        vista=Gtk.TreeView(model=modelo)
+        celda = Gtk.CellRendererText()
+        columna = Gtk.TreeViewColumn("Parentesco")
+        vista.append_column(columna)
+        columna.pack_start(celda,True)
+        columna.add_attribute(celda,"text",0)
+
+        columna2 = Gtk.TreeViewColumn("Orden",celda,text=1)
+        vista.append_column(columna2)
+
+        self.add(vista)
+        self.connect("delete_event", Gtk.main_quit)
+        self.show_all()
+
+if __name__=="__main__":
+    VentanaPrincipal()
+    Gtk.main()
